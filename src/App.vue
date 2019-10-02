af0cff34fb00a2a3c749413e2a0503929fed27d5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-10-01 19:50:21 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <x-greet />
+    <x-greet name="Mark" />
+    <x-counter />
+    <take-object title="Favorites" :obj.prop={favorites} />
+  </div>
+</template>
+
+<script>
+import './web-components.js';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      favorites: {color: 'yellow', number: 19}
+    }
+  }
+}
+</script>
